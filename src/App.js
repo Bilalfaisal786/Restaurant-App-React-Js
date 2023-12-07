3fe485a455e9a70f83220340fe6b5eddb964578b
Bilalfaisal786 <Bikily123@gmail.com>
2023-12-07 13:40:38 +0500

    completed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { AboutUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <Gallery />
+    <Footer />
+  </div>
+);
+
+export default App;
---
+++ src/container/index.js
@@ -0,0 +1,14 @@
+import AboutUs from './AboutUs/AboutUs';
+// import FindUs from './Findus/';
+import Footer from './Footer/Footer';
+import Gallery from './Gallery/Gallery';
+import Header from './Header/Header';
+
+
+export {
+  AboutUs,
+  // FindUs,
+  Footer,
+  Gallery,
+  Header,
+};
